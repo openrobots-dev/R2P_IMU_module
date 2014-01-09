c3c7b2d95eda84965c061d1ca6f4aaeff7820979
tinito <martino.migliavacca@gmail.com>
2014-01-09 16:24:40 +0100

    removed balance stuff

--- fw/main.cpp
+++ fw/main.cpp
@@ -65,121 +65,7 @@ r2p::Middleware r2p::Middleware::instance(R2P_MODULE_NAME, "BOOT_"R2P_MODULE_NAM
 RTCANConfig rtcan_config = { 1000000, 100, 60 };
 
 /*
- * Velocity control node
- */
-PID<float> vel_pid(5.0, 10.0, 0.5, -8.0, 8.0);
-//PID<float> vel_pid(1.0, 0.0, 0.0, -8.0, 8.0);
-//PID<float> vel_pid(5.0, 20.0, 0.0, -8.0, 8.0);
-float vel_setpoint = 0;
-float w_setpoint = 0;
-float angle_setpoint = 0;
-
-bool qei_callback(const r2p::QEIMsg &msg) {
-	float v;
-
-	palTogglePad(LED3_GPIO, LED3);
-	v = (msg.delta / (165500.0f)) * 20; // ->m/s @20hz
-	angle_setpoint = vel_pid.update(v, 0.05); // 20hz
-
-	return true;
-}
-
-msg_t velocity_node(void *arg) {
-	r2p::Node node("velocity");
-	r2p::Velocity3Msg vel_msgbuf[5], *vel_queue[5];
-	r2p::Subscriber<r2p::Velocity3Msg> vel_sub(vel_queue, 5);
-	r2p::Velocity3Msg *velp;
-	r2p::QEIMsg qei_msgbuf[5], *qei_queue[5];
-	r2p::Subscriber<r2p::QEIMsg> qei_sub(qei_queue, 5);
-	r2p::QEIMsg *qeip;
-	float v;
-
-	(void) arg;
-	chRegSetThreadName("velocity");
-
-	node.subscribe(qei_sub, "qei", qei_msgbuf);
-	node.subscribe(vel_sub, "velocity", vel_msgbuf);
-
-	vel_pid.set(0);
-
-	for (;;) {
-		if (!node.spin(r2p::Time::ms(500))) {
-			angle_setpoint = 0;
-			continue;
-		}
-
-		while (qei_sub.fetch(qeip)) {
-			palTogglePad(LED3_GPIO, LED3);
-			v = (qeip->delta / (165500.0f)) * -20.0; // ->m/s @20hz
-			angle_setpoint = vel_pid.update(v, 0.05); // 20hz
-			qei_sub.release(*qeip);
-		}
-
-		while (vel_sub.fetch(velp)) {
-			palTogglePad(LED2_GPIO, LED2);
-			vel_setpoint = velp->x;
-			w_setpoint = velp->w;
-			vel_sub.release(*velp);
-		}
-		vel_pid.set(vel_setpoint);
-
-	}
-	return CH_SUCCESS;
-}
-
-/*
- * Balance control node
- */
-msg_t balance_node(void *arg) {
-	r2p::Node node("balance");
-
-	r2p::TiltMsg sub_msgbuf[2], *sub_queue[2];
-	r2p::Subscriber<r2p::TiltMsg> tilt_sub(sub_queue, 2);
-	r2p::TiltMsg *tiltp;
-
-	r2p::Publisher<r2p::PWM2Msg> pwm2_pub;
-	r2p::PWM2Msg *pwmp;
-
-	int32_t pwm = 0;
-
-	(void) arg;
-	chRegSetThreadName("balance");
-
-	node.advertise(pwm2_pub, "pwm2");
-	r2p::Thread::sleep(r2p::Time::ms(43));
-
-	node.subscribe(tilt_sub, "tilt", sub_msgbuf);
-	r2p::Thread::sleep(r2p::Time::ms(71));
-
-	PID<float> pid(600, 1000, 100, -2000, 2000);
-	//PID<float> pid(400, 0, 0, -1000, 1000);
-	pid.set(angle_setpoint);
-
-	for (;;) {
-		pid.set(angle_setpoint);
-
-		while (!tilt_sub.fetch(tiltp)) {
-			r2p::Thread::sleep(r2p::Time::ms(1));
-		}
-
-		pwm = pid.update(tiltp->angle, 0.01); //rad2grad
-		tilt_sub.release(*tiltp);
-
-		if (pwm2_pub.alloc(pwmp)) {
-			pwmp->pwm1 = -(pwm + (w_setpoint * 100));
-			pwmp->pwm2 = pwm - (w_setpoint * 100);
-			pwm2_pub.publish(*pwmp);
-			palSetPad(LED4_GPIO, LED4);
-		} else {
-			palClearPad(LED4_GPIO, LED4);
-		}
-
-	}
-	return CH_SUCCESS;
-}
-
-/*
- * Madgwick node
+ * Publisher node
  */
 extern gyro_data_t gyro_data;
 extern acc_data_t acc_data;
@@ -196,14 +82,14 @@ static const EXTConfig extcfg = { { { EXT_CH_MODE_DISABLED, NULL }, { EXT_CH_MOD
 		EXT_CH_MODE_DISABLED, NULL }, { EXT_CH_MODE_DISABLED, NULL }, { EXT_CH_MODE_DISABLED, NULL }, {
 		EXT_CH_MODE_DISABLED, NULL }, { EXT_CH_MODE_DISABLED, NULL } } };
 
-msg_t madgwick_node(void *arg) {
-	r2p::Node node("madgwick");
-	r2p::Publisher<r2p::TiltMsg> tilt_pub;
-	attitude_t attitude_data;
+msg_t publisher_node(void *arg) {
+	r2p::Node node("imu_pub");
+	r2p::Publisher<r2p::tIMURaw9> imu_pub;
+	r2p::tIMURaw9 *msgp;
 	systime_t time;
 
 	(void) arg;
-	chRegSetThreadName("madgwick");
+	chRegSetThreadName("imu_pub");
 
 	i2cStart(&I2C_DRIVER, &i2c1cfg);
 	spiStart(&SPI_DRIVER, &spi1cfg);
@@ -211,58 +97,30 @@ msg_t madgwick_node(void *arg) {
 
 	gyroRun(&SPI_DRIVER, NORMALPRIO);
 	accRun(&I2C_DRIVER, NORMALPRIO);
-//	magRun(&I2C_DRIVER, NORMALPRIO);
+	magRun(&I2C_DRIVER, NORMALPRIO);
 
-	node.advertise(tilt_pub, "tilt");
+	node.advertise(imu_pub, "imuraw");
 
 	time = chTimeNow();
 
 	for (;;) {
-		MadgwickAHRSupdateIMU((gyro_data.x / 57.143) * 3.141592 / 180.0, (gyro_data.y / 57.143) * 3.141592 / 180.0,
-				(gyro_data.z / 57.143) * 3.141592 / 180.0, acc_data.x / 1000.0, acc_data.y / 1000.0,
-				acc_data.z / 1000.0);
-		getMadAttitude(&attitude_data);
+		if (imu_pub.alloc(msgp)) {
+			msgp->gyro_x = gyro_data.x;
+			msgp->gyro_y = gyro_data.y;
+			msgp->gyro_z = gyro_data.z;
+			msgp->acc_x = acc_data.x;
+			msgp->acc_y = acc_data.y;
+			msgp->acc_z = acc_data.z;
+			msgp->mag_x = mag_data.x;
+			msgp->mag_y = mag_data.y;
+			msgp->mag_z = mag_data.z;
 
-		r2p::TiltMsg *msgp;
-		if (tilt_pub.alloc(msgp)) {
-			msgp->angle = -((attitude_data.roll * 57.29578) + 5.2); // basketbot offset
-//			msgp->angle = -((attitude_data.roll * 57.29578));
-			tilt_pub.publish(*msgp);
+			imu_pub.publish(*msgp);
 		}
 
 		time += MS2ST(10);
 		chThdSleepUntil(time);
 	}
-	return CH_SUCCESS;
-}
-
-msg_t debug_node(void *arg) {
-	r2p::Node node("debug");
-
-	(void) arg;
-	chRegSetThreadName("debug");
-
-	sdStart(&SERIAL_DRIVER, NULL);
-
-#if 0
-	r2p::TiltMsg sub_msgbuf[2], *sub_queue[2];
-	r2p::Subscriber<r2p::TiltMsg> tilt_sub(sub_queue, 2);
-	r2p::TiltMsg *tiltp;
-
-	node.subscribe(tilt_sub, "tilt", sub_msgbuf);
-
-	for (;;) {
-		node.spin(r2p::Time::ms(500));
-
-		if (tilt_sub.fetch(tiltp)) {
-			chprintf((BaseSequentialStream*)&SERIAL_DRIVER, "tilt: %f\r\n", tiltp->angle);
-			tilt_sub.release(*tiltp);
-		} else {
-			chprintf((BaseSequentialStream*)&SERIAL_DRIVER, "timeout%d\r\n");
-		}
-	}
-#endif
-
 	return CH_SUCCESS;
 }
 
@@ -282,12 +140,8 @@ int main(void) {
 	r2p::Thread::create_heap(NULL, THD_WA_SIZE(512), NORMALPRIO + 1, r2p::ledsub_node, NULL);
 	uint8_t led = 1;
 	r2p::Thread::create_heap(NULL, THD_WA_SIZE(256), NORMALPRIO + 1, r2p::ledpub_node, (void *) &led);
-	r2p::Thread::create_heap(NULL, THD_WA_SIZE(1024), NORMALPRIO + 2, madgwick_node, NULL);
-	r2p::Thread::sleep(r2p::Time::ms(5000));
-	r2p::Thread::create_heap(NULL, THD_WA_SIZE(1024), NORMALPRIO + 2, balance_node, NULL);
-	r2p::Thread::sleep(r2p::Time::ms(500));
-	r2p::Thread::create_heap(NULL, THD_WA_SIZE(1024), NORMALPRIO + 2, velocity_node, NULL);
-//	r2p::Thread::create_heap(NULL, THD_WA_SIZE(1024), NORMALPRIO + 2, debug_node, NULL);
+	r2p::Thread::create_heap(NULL, THD_WA_SIZE(1024), NORMALPRIO + 2, publisher_node, NULL);
+
 	for (;;) {
 		r2p::Thread::sleep(r2p::Time::ms(500));
 	}
